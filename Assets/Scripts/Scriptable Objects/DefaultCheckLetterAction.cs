a08bcd95cedf33725fe8fee8aa28d67c55c3dd5f
Guizorzon <gui.zorzon@usp.br>
2022-09-12 10:05:03 -0300

    Create default check letter action

---
+++ Assets/Scripts/Scriptable Objects/DefaultCheckLetterAction.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptedAction", menuName = "Scripted Actions/New DefaultCheckLetterAction")]
+public class DefaultCheckLetterAction : ScriptableAction
+{
+    public override void CheckLetters(Mosquitoe mosquitoe, string letterToCheck)
+    {
+      if(mosquitoe.assignedText.Contains(letterToCheck) && !mosquitoe.isDestoyed)
+      {
+        if(mosquitoe.currentAssignedText.Count == 1)
+        {
+          mosquitoe.isDestoyed = true;
+          DestroyMosquitoe(mosquitoe);
+          MosquitoeSpawner.instance.mosquitoesToRemove.Add(mosquitoe);
+        }
+        mosquitoe.RemoveLetter(letterToCheck);
+      }
+    }
+}

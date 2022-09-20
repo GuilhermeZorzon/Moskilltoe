762cf07dbec28956053597310b3dffa607423adc
Guizorzon <gui.zorzon@usp.br>
2022-09-20 07:45:56 -0300

    Create ScriptableSound class to add sounds to game

---
+++ Assets/Scripts/Scriptable Objects/ScriptableSound.cs
@@ -0,0 +1,33 @@
+using Unity.Audio;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptedSound", menuName = "Scripted Sounds/New ScriptedSound")]
+public class ScriptableSound : ScriptableObject
+{
+    public AudioClip clip;
+    public float volume;
+    public float pitch;
+
+    private AudioSource audioSource = null;
+    
+    public AudioSource Play()
+    {
+        if (this.audioSource == null)
+        {
+            var audioSourceObj = new GameObject("AudioSource", typeof(AudioSource));
+            this.audioSource = audioSourceObj.GetComponent<AudioSource>();
+        }
+
+        this.audioSource.clip = this.clip;
+        this.audioSource.volume = this.volume;
+        this.audioSource.pitch = this.pitch;
+        this.audioSource.Play();
+
+        return audioSource;
+    }
+
+    public void Stop()
+    {
+        this.audioSource.Stop();
+    }
+}
